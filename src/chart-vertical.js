0007f5e790130912c1f454b19e0fb45c3c0ec81d
Kishor Mandal <kishor15mandal1999@gmail.com>
2023-03-18 07:29:04 +0530

    New

---
+++ src/chart-vertical.js
@@ -0,0 +1,109 @@
+import {useEffect, useState} from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+
+import { Bar } from 'react-chartjs-2';
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+const options = {
+    indexAxis: 'x',
+    elements: {
+      bar: {
+        borderWidth: 2,
+      },
+    },
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Vertical Bar Chart',
+      },
+    },
+  };
+
+const Horizontalchart =() => {
+    const [data, setData] = useState({
+        labels:[],
+        datasets: [
+          {
+            label: 'Age',
+            data:[],
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(25, 90, 13, 0.5)',
+          },
+          {
+            label: 'Fair',
+            data:[],
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+          },
+        ],
+      });
+    useEffect(()=> {
+       const fetchData= async()=> {
+           const url = 'https://jsonhero.io/j/zMPfaDIHw6ez.json'
+           const labelSet = []
+           const dataSet1 = [];
+           const dataSet2 = [];
+         await fetch(url).then((data)=> {
+             console.log("Api data", data)
+             const res = data.json();
+             return res
+         }).then((res) => {
+             console.log("ressss", res)
+            for (const val of res) {
+                dataSet1.push(val.data);
+                dataSet2.push(val);
+                // labelSet.push(val.name)
+            }
+            setData({
+                labels:[],
+                datasets: [
+                  {
+                    label: 'Age',
+                    data:dataSet1,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(99, 132, 0.5)',
+                  },
+                  {
+                    label: 'Fair',
+                    data:dataSet2,
+                    borderColor: 'rgb(53, 162, 235)',
+                    backgroundColor: 'rgba(53, 235, 0.5)',
+                  },
+                ],
+              })
+            console.log("arrData", dataSet1, dataSet2)
+         }).catch(e => {
+                console.log("error", e)
+            })
+        }
+        
+        fetchData();
+    },[])
+   
+    return(
+        <div style={{width:'80%', height:'50%'}}>
+            {
+                console.log("dataaaaaaaa", data)
+            }
+            <Bar data={data} options={options}/>
+         </div>)
+}
+export default Horizontalchart;
